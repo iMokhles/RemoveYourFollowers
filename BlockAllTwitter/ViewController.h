3f4c8ff7afcf98ca0cca1c730ed2f5e5adf65c07
iMokhles <mokhleshussien@aol.com>
2017-02-21 17:48:58 +0100

    Initial Commit

---
+++ BlockAllTwitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlockAllTwitter
+//
+//  Created by iMokhles on 21/02/2017.
+//  Copyright © 2017 iMokhles. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ BlockAllTwitter/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  BlockAllTwitter
+//
+//  Created by iMokhles on 21/02/2017.
+//  Copyright © 2017 iMokhles. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
